1fa99fe0a943116ca62a4858ac442053866fb80c
pat-s <patrick.schratz@gmail.com>
2019-04-09 21:47:23 +0200

    try macro

--- tic.R
+++ tic.R
@@ -1,13 +1,1 @@
-get_stage("install") %>%
-  add_code_step(remotes::install_deps(dependencies = TRUE))
-
-get_stage("deploy") %>%
-  add_code_step(step_build_bookdown())
-
-if (ci_has_env("id_rsa")) {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_step(step_push_deploy(path = "_book", branch = "gh-pages"))
-}
+do_build_bookdown()
