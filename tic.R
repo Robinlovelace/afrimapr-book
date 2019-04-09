fdc0cd10237b38abdf31f5af80c0893e369ed984
pat-s <patrick.schratz@gmail.com>
2019-04-09 21:24:05 +0200

    update tic.R and .travis.yml

--- tic.R
+++ tic.R
@@ -1,21 +1,10 @@
-get_stage("before_install") %>%
-  add_code_step(update.packages(ask = FALSE))
-
 get_stage("install") %>%
   add_code_step(remotes::install_deps(dependencies = TRUE))
 
 get_stage("deploy") %>%
-  add_code_step(
-    bookdown::render_book('index.Rmd', 'bookdown::gitbook'),
-    prepare_call = remotes::install_github("rstudio/bookdown")
-  )
+  add_code_step(step_build_bookdown())
 
-if (Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+if (ci_has_env("id_rsa")) {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
